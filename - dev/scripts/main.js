efeaacbf4ea25bd333424f62a64e4a15f18e4236
thesiv95 <091995_mail@mail.ru>
2015-11-01 00:20:58 +0600

    rating

--- - dev/scripts/main.js
+++ - dev/scripts/main.js
@@ -4,16 +4,12 @@ var SliderWidget = (function(){
     var _insertValues = function($this) {
 
         var
-            container = $this.closest('.filter__slider'),
-            from = container.find('.filter__slider-input-from'),
-            to = container.find('.filter__slider-input-to');
+            from = $('.filter__slider-input_from'),
+            to = $('.filter__slider-input_to');
 
         var values = $this.slider('option', 'values');
-            console.log(values);
 
-        from.value = values[0];
-
-        //from.val(values[0]);
+        from.val(values[0]);
         to.val(values[1]);
 
     }; // end sliderwidget.insertValues
@@ -25,8 +21,8 @@ var SliderWidget = (function(){
             $('.filter__slider-element').each(function(){
                 var
                     $this = $(this),
-                    min = parseInt($(this).data('min')),
-                    max = parseInt($(this).data('max'));
+                    min = parseInt($this.data('min')),
+                    max = parseInt($this.data('max'));
 
                 $this.slider({
                     range: true,
@@ -47,15 +43,77 @@ var SliderWidget = (function(){
 }()); // end sliderwidget
 
 
+var RatingWidget = (function(){
 
+    var _starsShiningBright = function(ratingAmount) {
+        var
+            starsArray = [];
+
+        for (var i = 0; i < 5; i++) {
+            var
+                starClassName = (i < ratingAmount) ? 'products__rating-stars-item products__rating-stars-item_filled' : 'products__rating-stars-item';
+
+            var star = $('<li>', {
+                class: starClassName
+            });
+
+
+            starsArray.push(star);
+
+        }
+
+
+        return starsArray;
+
+    };
+
+
+    var _markupGen = function(ratingAmount, elementToAppend) {
+        var
+            ul = $('<ul>', {
+                class: 'products__rating-stars',
+                html: _starsShiningBright(ratingAmount)
+            });
+
+        var
+            ratingDisplay = $('<div>', {
+                class: 'products__rating-amount',
+                text: ratingAmount
+            });
+
+        elementToAppend.append([ul, ratingDisplay]);
+
+    };
+
+    return {
+        init: function(){
+            $('.products__rating').each(function(){
+                var
+                    $this = $(this),
+                    ratingAmount = parseInt($(this).data('rating'));
+
+                _markupGen(ratingAmount, $this);
+            });
+        }
+    }
+
+}()); // end ratingwidget
 
 
 /*********** Initialize  ***********/
 
 $(document).ready(function() {
 
+    if($('.products__rating').length) {
+        RatingWidget.init();
+    }
+
     if($('.filter__slider-element').length) {
         SliderWidget.init();
+    }
+
+    if($('.sort__select-elem').length) {
+        
     }
 
 }); //-> ready end
